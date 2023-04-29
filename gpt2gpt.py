72c1a72b6a234f5c00227a8d6920f49e221bd372
Marco Kotrotsos <kotrotsos@gmail.com>
2023-04-29 23:43:30 +0200

    added agent role

--- gpt2gpt.py
+++ gpt2gpt.py
@@ -11,8 +11,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def get_gpt3_response(messages, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
-        messages=messages,
+        messages=messages
     )
+
     return response.choices[0].message['content'].strip()
 
 def append_to_file(filename, text):
@@ -27,6 +28,18 @@ def load_topic(topic_file):
 def save_topic(topic_file, topic):
     with open(topic_file, 'w') as f:
         f.write(topic)
+
+def get_agent_input(last_message, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": last_message}]
+    agent_prompt = {"role": "assistant", "content": "As an agent, provide a helpful suggestion based on the last message in the conversation. If you can't provide any assistance, respond with 'No suggestion'."}
+    messages.append(agent_prompt)
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+    )
+    return response.choices[0].message['content'].strip()
+
 def main(participant1, participant2, model):
     topic_file = "topic.txt"
 
@@ -46,7 +59,7 @@ def main(participant1, participant2, model):
     print(f"{participant2}: {initial_message}")
     append_to_file(filename, f"{participant2}: {initial_message}")
 
-    conversation_history = deque(maxlen=10)
+    conversation_history = deque(maxlen=30)
     conversation_history.append({"role": "system", "content": "You are a very educated, outgoing person and engaging person. You provide structured helpful answers and provide followup questions when necessary. Your goal is to come up with as many helpful discussion topics as possible and to keep the converation going but showing insight, and opening new conversational avanues. "})
     conversation_history.append({"role": "user", "content": initial_message})
 
@@ -75,13 +88,19 @@ def main(participant1, participant2, model):
         mod_counter += 1
         if mod_counter % 3 == 0:
             mod_prompt = list(conversation_history.copy())
-            mod_prompt.append({"role": "assistant", "content": "As a moderator, provide some extra helpful information that adds to the discussion. For instance, it maybe poses a new question that hasn't been discussed yet. Also, if you think the conversation Is not going anywhere, like when the conversation constantly repeats itself and should end. Reply only with [END]. Mod:"})
+            mod_prompt.append({"role": "assistant", "content": "As a moderator, provide some extra helpful information that adds to the discussion. For instance, it maybe poses a new question that hasn't been discussed yet. Also, if you think the conversation Is not going anywhere, reply only with [END]. Mod:"})
             mod_response = get_gpt3_response(mod_prompt, "gpt-3.5-turbo")
             print(f"#Moderator: {mod_response}")
             append_to_file(filename, f"#Moderator: {mod_response}")
             conversation_history.append({"role": "user", "content": mod_response})
             time.sleep(3)
 
+# Agent input
+        last_message = conversation_history[-1]['content']
+        agent_suggestion = get_agent_input(last_message, model)
+        if agent_suggestion != "No suggestion":
+            print(f"#Agent: {agent_suggestion}")
+            append_to_file(filename, f"#Agent: {agent_suggestion}")
      
         
 
